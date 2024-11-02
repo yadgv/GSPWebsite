b8de7c335665bfcc3bc47e98ecfe84b361bee623
Ziad <zabdelgayed@gmail.com>
2024-11-02 10:02:29 -0400

    modified:   ToBeDetermined-main/design.css 	modified:   ToBeDetermined-main/home.html 	modified:   ToBeDetermined-main/java.js 	renamed:    ToBeDetermined-main/sources/Walle LightOff Left.png -> ToBeDetermined-main/sources/WalleLightOffLeft.png 	renamed:    ToBeDetermined-main/sources/Walle LightOn Left.png -> ToBeDetermined-main/sources/WalleLightOnLeft.png 	renamed:    ToBeDetermined-main/sources/Walle LightOn Right.png -> ToBeDetermined-main/sources/WalleLightOnRight.png

--- ToBeDetermined-main/java.js
+++ ToBeDetermined-main/java.js
@@ -1,15 +1,27 @@
+var lightOn = false;
+document.onclick = function() {
+  lightOn = true;
+  walle = document.getElementById("homeWalle")
+  walle.src = "sources/WalleLightOnLeft.png";
+  walle.style.opacity = "0.7";
+};
 document.addEventListener("DOMContentLoaded", function () {
     const light = document.createElement("div");
     light.id = "light";
     document.body.appendChild(light);
-  
+    light.style.opacity = "0";
+
     document.addEventListener("mousemove", function (e) {
-      const x = e.clientX;
-      const y = e.clientY;
-      light.style.left = x + "px";
-      light.style.top = y + "px";
+      if (lightOn) {
+        light.style.opacity = "1";
+        const x = e.clientX;
+        const y = e.clientY;
+        light.style.left = x + "px";
+        light.style.top = y + "px";
+      }
     });
 });
+
 
 // Get the modal
 var modal = document.getElementById("imageModal");
