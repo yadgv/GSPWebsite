8947f32c5459e0b5c7765b93db18a4bfb956c660
Ziad <zabdelgayed@gmail.com>
2024-11-11 10:26:59 -0500

    modified:   ToBeDetermined-main/aboutthegame.html 	modified:   ToBeDetermined-main/aboutus.html 	modified:   ToBeDetermined-main/design.css 	modified:   ToBeDetermined-main/gallery.html 	modified:   ToBeDetermined-main/java.js

--- ToBeDetermined-main/java.js
+++ ToBeDetermined-main/java.js
@@ -60,46 +60,72 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     }
   });
-});
 
+  if (document.URL.includes("gallery.html")) {
+    // Get the modal
+    var modal = document.getElementById("imageModal");
 
+    // Get the modal image element and close button
+    var modalImg = document.getElementById("modalImg");
+    var closeBtn = document.getElementsByClassName("close")[0];
 
+    // Add click event to each image in the gallery
+    var images = document.querySelectorAll('.gallery-card img');
+    images.forEach(function(image) {
+        image.onclick = function(){
+            modal.style.display = "block";
+            modalImg.src = this.src;
+        };
+    });
 
-
-
-
-// Get the modal
-var modal = document.getElementById("imageModal");
-
-// Get the modal image element and close button
-var modalImg = document.getElementById("modalImg");
-var closeBtn = document.getElementsByClassName("close")[0];
-
-// Add click event to each image in the gallery
-var images = document.querySelectorAll('.gallery-card img');
-images.forEach(function(image) {
-    image.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
+    // Close the modal when user clicks the "X" button
+    closeBtn.onclick = function() {
+        modal.style.display = "none";
     };
-});
 
-// Close the modal when user clicks the "X" button
-closeBtn.onclick = function() {
-    modal.style.display = "none";
-};
+    // Close the modal when the user clicks outside the image
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    };
 
-// Close the modal when the user clicks outside the image
-window.onclick = function(event) {
-    if (event.target === modal) {
+    document.addEventListener('keydown', function(event) {
+      if (event.key === "Escape") { // Check if the pressed key is Esc
         modal.style.display = "none";
     }
-};
 
-document.addEventListener('keydown', function(event) {
-  if (event.key === "Escape") { // Check if the pressed key is Esc
-    modal.style.display = "none";
+    });
+
+  }
+  if (document.URL.includes("aboutthegame.html")){
+    const previewImg = document.getElementById("previewImg");
+    const photos = document.querySelectorAll('.photoPickerImg');
+  
+    photos.forEach(function(image) {
+      image.addEventListener("click", function(event) {
+        previewImg.src = this.src;
+      });
+    });
+
+    const buttonRight = document.getElementById('rightButton');
+    const buttonLeft = document.getElementById('leftButton');
+    
+    const photoPicker = document.getElementsByClassName('photoPicker')[0];
+    
+    buttonRight.onclick = function () {
+      photoPicker.scrollLeft += 400;
+    };
+    
+    buttonLeft.onclick = function () {
+      photoPicker.scrollLeft -= 400;
+    };
   }
 });
+
+
+
+
+
 
 
